69aea334282f40dadc7095dee2a85aef23912f87
RaghavGupta13 <raghav.gupta13@outlook.com>
2020-07-25 15:35:29 -0500

    converted queue to stack

--- Stack_Example/src/Display.java
+++ Stack_Example/src/Display.java
@@ -5,13 +5,13 @@ public class Display {
 	public static void main(String[] args) {
 		LinkedList<String> names = new LinkedList<String> (); 
 		
-		names.add("John");
-		names.add("Michael");
-		names.add("Jay");
+		names.push("John");
+		names.push("Michael");
+		names.push("Jay");
 		
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-		System.out.println(names.remove());
+		System.out.println(names.pop());
+		System.out.println(names.pop());
+		System.out.println(names.pop());
 	}
 
 }
